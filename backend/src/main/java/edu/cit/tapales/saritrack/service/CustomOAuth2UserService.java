5212e94ec057b2cfa0cb1dad3b37ad5083151a15
Christian Tapales <christian.tapales@cit.edu>
2026-04-13 10:05:17 +0800

    feat: implement custom oauth2 user service for persistence

---
+++ backend/src/main/java/edu/cit/tapales/saritrack/service/CustomOAuth2UserService.java
@@ -0,0 +1,43 @@
+package edu.cit.tapales.saritrack.service;
+
+import edu.cit.tapales.saritrack.entity.User;
+import edu.cit.tapales.saritrack.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        // 1. Fetch the user info from Google
+        OAuth2User googleUser = super.loadUser(userRequest);
+        
+        String email = googleUser.getAttribute("email");
+        String name = googleUser.getAttribute("name");
+
+        // 2. Sync with Supabase: Save user if they don't exist
+        Optional<User> existingUser = userRepository.findByEmail(email);
+        
+        if (existingUser.isEmpty()) {
+            User newUser = new User();
+            newUser.setEmail(email);
+            newUser.setName(name);
+            // OAuth users don't need a password field filled, but your entity 
+            // requires it. Set a random one or modify the entity.
+            newUser.setPassword("OAUTH_USER"); 
+            userRepository.save(newUser);
+        }
+
+        return googleUser;
+    }
+}
