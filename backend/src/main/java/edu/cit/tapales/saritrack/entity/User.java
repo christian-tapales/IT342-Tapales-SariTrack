55f6db27fbeeeeddf50c3a6d8bbe8b0a68b055a2
Christian Tapales <christian.tapales@cit.edu>
2026-04-13 10:05:17 +0800

    feat: implement custom oauth2 user service for persistence

--- backend/src/main/java/edu/cit/tapales/saritrack/entity/User.java
+++ backend/src/main/java/edu/cit/tapales/saritrack/entity/User.java
@@ -16,7 +16,7 @@ public class User {
     @Column(unique = true, nullable = false)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String password;
 
     private String role = "VENDOR"; // Default role
